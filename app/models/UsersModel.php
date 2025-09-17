933fe1908dd23195b3d8378bbb06c763bfff7848
Pastrana Vina <pastranavina@gmail.com>
2025-09-17 11:46:09 +0800

    First Commit

--- app/models/UsersModel.php
+++ app/models/UsersModel.php
@@ -7,7 +7,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
  * Automatically generated via CLI.
  */
 class UsersModel extends Model {
-    protected $table = 'user';
+    protected $table = 'users';
     protected $primary_key = 'id';
 
     public function __construct()
